b7a4f93bd649cce79320278116c5557e243af081
Prgrmmr633 <airesmacalino09@gmail.com>
2024-02-03 00:58:30 +0800

    test

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
-from fastapi import FastAPI, Form, UploadFile, Request  # , WebSocket
+from fastapi import FastAPI, Form, UploadFile, Request, Response, status
 from fastapi.templating import Jinja2Templates
+import uvicorn
 import requests
 import os
 
 templates = Jinja2Templates(directory=".")
-
-headers = {"Api-Key": os.getenv("5675fd21-7929-2597-6c8e-69e220ede9a2")}
-
 app = FastAPI()
 
 @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+@app.head("/", status_code=200)
+async def health(response: Response):
+    response.status_code = status.HTTP_200_OK
+    return
 
-@app.post("/img/")
+@app.post("/image")
 async def process(
     *,
-    age: int = Form(...),
-    sex: str = Form(...),
-    image_uploads: UploadFile = Form(...),
+    image: UploadFile = Form(...),
     request: Request,
 ):
-
-    image_contents = await image_uploads.read()
-    received_file = {"Image": image_contents}
-
+    image_contents = await image.read()
     response = requests.post(
-        os.getenv("API_URL", "https://autoderm-api.firstderm.com/Query"),
-        headers=headers,
-        files=received_file,
-        data={"AgeYears": age, "Sex": sex, "Language": "EN", "Model": "43PLUS_noo_v3"},
-    )
+         "https://autoderm.ai/v1/query?model=autoderm_v2_0&language=en",
+         headers={"Api-Key": "5675fd21-7929-2597-6c8e-69e220ede9a2"},
+         files={"file": image_contents},
+     )
 
     data = response.json()
-
     predictions = data["predictions"]
 
     return templates.TemplateResponse(
         "prediction.html", {"request": request, "predictions": predictions}
     )
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=3000, reload=False)
