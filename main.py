4a962b2f71429ea33f028f2a18b8f6b8e8720d23
9QIX <kirkhendrixbulaong@outlook.com>
2024-02-11 18:40:16 +0800

    Refactor code to save predictions to the database

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ import uvicorn
 import requests
 import os
 
-
 templates = Jinja2Templates(directory=".")
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
 
 # Pydantic BaseModels
 class Prediction(BaseModel):
@@ -23,12 +23,15 @@ class Prediction(BaseModel):
     confidence: float
     read_more_url: str
 
+
 class Predictions(BaseModel):
     predictions: List[Prediction]
+
 
 class UserInput(BaseModel):
     image: str
     prediction_id: int
+
 
 # Connect database
 def get_db():
@@ -37,6 +40,11 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# Create annotation for dependency
+db_dependency = Annotated[Session, Depends(get_db)]
+
 
 # API Endpoints
 @app.get("/")
@@ -69,6 +77,13 @@ async def process(
     # Extract confidence scores from predictions
     confidence_scores = [prediction.get("confidence", 0) for prediction in predictions]
 
+    # Save predictions to the database
+    db = SessionLocal()
+    for prediction in predictions:
+        db_prediction = Prediction(name=prediction['name'], confidence=prediction['confidence'])
+        db.add(db_prediction)
+    db.commit()
+
     return templates.TemplateResponse(
         "prediction.html", {"request": request, "predictions": predictions, "confidence_scores": confidence_scores}
     )
@@ -76,3 +91,43 @@ async def process(
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=3100, reload=False)
+
+
+# Back up ----------------------------------------------------------------------------
+# @app.get("/")
+# async def index(request: Request):
+#     return templates.TemplateResponse("index.html", {"request": request})
+
+
+# @app.head("/", status_code=200)
+# async def health(response: Response):
+#     response.status_code = status.HTTP_200_OK
+#     return
+
+
+# @app.post("/image")
+# async def process(
+#         *,
+#         image: UploadFile = Form(...),
+#         request: Request,
+# ):
+#     image_contents = await image.read()
+#     response = requests.post(
+#         "https://autoderm.ai/v1/query?model=autoderm_v2_2&language=en",
+#         headers={"Api-Key": "5675fd21-7929-2597-6c8e-69e220ede9a2"},
+#         files={"file": image_contents},
+#     )
+
+#     data = response.json()
+#     predictions = data.get("predictions", [])
+
+#     # Extract confidence scores from predictions
+#     confidence_scores = [prediction.get("confidence", 0) for prediction in predictions]
+
+#     return templates.TemplateResponse(
+#         "prediction.html", {"request": request, "predictions": predictions, "confidence_scores": confidence_scores}
+#     )
+
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="localhost", port=3100, reload=False)
